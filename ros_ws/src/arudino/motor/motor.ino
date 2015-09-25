da067f38fa797c2d1e90168c59a3380bdc07d342
Ryan Loffelman <ryanloffelman@me.com>
2015-09-25 11:53:40 -0500

    Fix all issues except the include
    Fixed all the issues you came up with however code will not run due to
    needing to include a file that it can't find.

--- ros_ws/src/arudino/motor/motor.ino
+++ ros_ws/src/arudino/motor/motor.ino
@@ -1,20 +1,33 @@
 /**
  * @file motor.ino
  * @brief Controls the Motors
+ * 
+ * Code to be put on the Arduino based Motor Controllers
+ * Converts Angular Velocity (Rad/Sec) to intiger (0-255)
+ * 
  */
 
 #include <ros.h>
 #include <std_msgs/Float64.h>
-
-#define pi 3.14
+#include <common/constants.h>
 
 /*************
  * Constants *
  *************/
- //Radians Per Second at full Throtall
-const float MAX_RAD_SEC = (8*pi);
+ 
+/**
+ * @brief Define Pi for use in conversion
+ */
+const float PI_CONST = 3.14159265359;
+ 
+/**
+ * @brief Radians Per Second at full Throtall
+ */
+const float MAX_RAD_SEC = (8*PI_CONST);
 
-//Pins used to controll the Motor
+/**
+ * @brief Pins used to controll the Motor
+ */
 const int FORWARD_PWM_PIN = 3;
 const int REVERSE_PWM_PIN = 4;
 const int ENABLE_PIN = 5;
@@ -22,63 +35,70 @@ const int ENABLE_PIN = 5;
 /************************
  * Forward Declerations *
  ************************/
-void VelocityCallback(const std_msgs::Float64 &msg);
+void velocityCallback(const std_msgs::Float64& msg);
 
-//ROS node handle
+/**
+ * @brief ROS node handle
+ */
 ros::NodeHandle nodeHandle;
 
-//ROS Subscriber for the Velocity Message
-ros::Subscriber<std_msgs::Float64> 
-     velocitySub("wheel_velocity", &VelocityCallback);
+/**
+ * @brief ROS Subscriber for the Velocity Message
+ */
+ros::Subscriber<std_msgs::Float64> velocitySub(VELOCITY_TOPIC, &VelocityCallback);
 
-/*************
- * Callbacks *
- *************/
-void VelocityCallback(const std_msgs::Float64 &msg)
+/**
+ * @brief The callback function for velocity messages
+ * 
+ * This function accepts velocity messages and convertes them into wheel speeds
+ * This will convert a float into an intger(0-255)
+ * 
+ * @param msg The message witch is recived from the velocity publisher
+ */
+void velocityCallback(const std_msgs::Float64& msg)
 {
-  int speed;
-  if(msg.data > 0)
-  {
-    speed = map(msg.data, 0, MAX_RAD_SEC, 0, 255);
-    //Go Forward
-    digitalWrite(ENABLE_PIN, HIGH);
-    analogWrite(FORWARD_PWM_PIN, speed);
-    analogWrite(REVERSE_PWM_PIN, 0);
-  }
-  else if(msg.data < 0)
-  {
-    speed = map(abs(msg.data), 0, MAX_RAD_SEC, 0, 255);
-    //Go in Reverse
-    digitalWrite(ENABLE_PIN, HIGH);
-    analogWrite(FORWARD_PWM_PIN, 0);
-    analogWrite(REVERSE_PWM_PIN, speed);
-  }
-  else
-  {
-    //active break
-    digitalWrite(ENABLE_PIN, HIGH);
-    analogWrite(FORWARD_PWM_PIN, 0);
-    analogWrite(REVERSE_PWM_PIN, 0);
-  }
-  
+    int speed = map(abs(msg.data), 0, MAX_RAD_SEC, 0, 255);
+    if(msg.data > 0)
+    {
+        //Go Forward
+        digitalWrite(ENABLE_PIN, HIGH);
+        analogWrite(FORWARD_PWM_PIN, speed);
+        analogWrite(REVERSE_PWM_PIN, 0);
+    }
+    else if(msg.data < 0)
+    {
+        //Go in Reverse
+        digitalWrite(ENABLE_PIN, HIGH);
+        analogWrite(FORWARD_PWM_PIN, 0);
+        analogWrite(REVERSE_PWM_PIN, speed);
+    }
+    else
+    {
+        //active break
+        digitalWrite(ENABLE_PIN, HIGH);
+        analogWrite(FORWARD_PWM_PIN, 0);
+        analogWrite(REVERSE_PWM_PIN, 0);
+    }
 }
 
-void setup() {
-  //setup pins
-  pinMode(FORWARD_PWM_PIN, OUTPUT);
-  pinMode(REVERSE_PWM_PIN, OUTPUT);
-  pinMode(ENABLE_PIN, OUTPUT);
+void setup() 
+{
+    //setup pins
+    pinMode(FORWARD_PWM_PIN, OUTPUT);
+    pinMode(REVERSE_PWM_PIN, OUTPUT);
+    pinMode(ENABLE_PIN, OUTPUT);
 
-  //Set Robot to break when starting
-  digitalWrite(ENABLE_PIN, HIGH);
-  analogWrite(FORWARD_PWM_PIN, 0);
-  analogWrite(REVERSE_PWM_PIN, 0);
+    //Set Robot to break when starting
+    digitalWrite(ENABLE_PIN, HIGH);
+    analogWrite(FORWARD_PWM_PIN, 0);
+    analogWrite(REVERSE_PWM_PIN, 0);
 
-  //Setup ROS node and topics
-  nodeHandle.initNode();
-  nodeHandle.subscribe(velocitySub);
+    //Setup ROS node and topics
+    nodeHandle.initNode();
+    nodeHandle.subscribe(velocitySub);
 }
 
-void loop() {
-  nodeHandle.spinOnce();
+void loop() 
+{
+    nodeHandle.spinOnce();
 }
