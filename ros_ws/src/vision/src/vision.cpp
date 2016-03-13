966e2bf9eab8efd4c86aaf69fc4c22398deadf1b
MST Robotics <mstrobotics@mst.edu>
2016-03-13 13:31:18 -0500

    Add comments and proper syntax, Also fixed wierd exception from happening

--- ros_ws/src/vision/src/vision.cpp
+++ ros_ws/src/vision/src/vision.cpp
@@ -6,18 +6,20 @@
 
 #include "vision.h"
 
+//Default Constructor
 Vision::Vision() : it(nh) 
 {
-    visionSub = it.subscribe("/usb_cam/image_raw", 1, &Vision::imageCallback, this);
-    visionPub = it.advertise("processed_image", 1);
+    //Initialize Publishers and Subscribers
+    vision_sub = it.subscribe("/usb_cam/image_raw", 1, &Vision::imageCallback, this);
+    vision_pub = it.advertise("processed_image", 1);
 }
 
+//ROS Callback Function to get camera data
 void Vision::imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
 	//Get the image from the camera, and store it in frame
 	try
 	{
-		//Switch back to MONO8
 		frame = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::RGB8);
 	}
 	//Throw an error message if the camera data isn't correct
@@ -27,12 +29,27 @@ void Vision::imageCallback(const sensor_msgs::ImageConstPtr& msg)
 		return;
 	}
 
+    edgeDetection();
+    
     return;
 }
 
+//Function used to process the image
+void Vision::edgeDetection()
+{
+
+    return;
+}
+
+//Functions used to update ROS with the processed image
 void Vision::update()
 {
-    visionPub.publish(frame->toImageMsg());
+    //There is a delay in the beginning where this pointer is set to NULL, and will cause a spooky exception
+    //   This check should stop this from happening
+    if(frame != NULL)
+    {
+        vision_pub.publish(frame->toImageMsg());
+    }
 
     return;   
 }
--- ros_ws/src/vision/src/vision_node.cpp
+++ ros_ws/src/vision/src/vision_node.cpp
@@ -7,16 +7,21 @@
 
 int main(int argc, char** argv)
 {
+    //Setup ROS
     ros::init(argc, argv, "vision_node");
 
     Vision v;
 
+    //Set the rate to 30 Hertz
     ros::Rate loop_rate(30);
 
+    //While ROS is running
     while(ros::ok())
     {
+        //Check the Callbacks
         ros::spinOnce();
-        loop_rate.sleep();
-        v.update();    
+        loop_rate.sleep();   
+        //Update ROS with the processed message
+        v.update(); 
     }
 }
