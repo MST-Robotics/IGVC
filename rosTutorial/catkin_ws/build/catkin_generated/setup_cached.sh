960f6834e64c28d4e78937dfda0ada2e7f23f23f
MSTRobotics <mstrobotics@mst.edu>
2016-09-21 13:10:01 -0500

    Ros tutorials 1-5 added in rosTutorial folder

---
+++ rosTutorial/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PKG_CONFIG_PATH="/home/robotics/ChrisEitutis/IGVC/rosTutorial/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/robotics/ChrisEitutis/IGVC/rosTutorial/catkin_ws/build"
---
+++ rosTutorial/catkin_ws/build/rostutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robotics/ChrisEitutis/IGVC/rosTutorial/catkin_ws/src/rostutorial
+# Build directory: /home/robotics/ChrisEitutis/IGVC/rosTutorial/catkin_ws/build/rostutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
