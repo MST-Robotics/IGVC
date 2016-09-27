017b838dbbe1a41f097cbda1b4c44030978eb6b6
Jacob Kauffmann <jacobgkau@gmail.com>
2016-09-27 15:02:36 -0500

    Did tutorials 1-6 and 11, 13

---
+++ tutorial/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jacob/IGVC/tutorial/catkin_ws/src
+# Build directory: /home/jacob/IGVC/tutorial/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
---
+++ tutorial/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PWD="/home/jacob/IGVC/tutorial/catkin_ws/build"
+export PYTHONPATH="/home/jacob/IGVC/tutorial/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
